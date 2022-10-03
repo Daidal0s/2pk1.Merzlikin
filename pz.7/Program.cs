50236132138235cb487c9f97e99c1b84408bf9c7
StrvngeMan21 <strangeman21@outlook.com>
2022-10-03 10:27:29 +0300

    Убрал некомпетентный комментари

--- pz.7/Program.cs
+++ pz.7/Program.cs
@@ -12,8 +12,6 @@ namespace pz._7
             for (short i = 0; i < arr.GetUpperBound(0); i++)
                 arr[i] = (short)rnd.Next(0, 51);
 
-            // Из-за того, что на компьютере в 407 проблемы с .NET, метод Array.Sort() мне не доступен. Пришлось сортировать так)
-
             for (short i = 0, tempOne = 0; i <= arr.GetUpperBound(0); i++)
             {
                 for (short j = (short)(i + 1); j <= arr.GetUpperBound(0); j++)
@@ -27,7 +25,7 @@ namespace pz._7
                 }
             }
 
-            for (short i = 0, temp = 1; i <= arr.GetUpperBound(0); i++, temp++)
+            for (short i = 0, temp = 1; i <= arr.GetUpperBound(0); i++, temp++)             // Я не забыл про foreach, я не хотел объявлять лишнюю переменную вне цикла)
             {
                 Console.WriteLine($"Элемент массива {temp} = {arr[i]}\tСумма цифр = {(arr[i] % 10) + (arr[i] / 10)}");
             }
