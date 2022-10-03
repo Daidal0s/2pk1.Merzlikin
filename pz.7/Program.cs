3c5a460308fe95ccf047c4b197a2d6190b362435
StrvngeMan21 <strangeman21@outlook.com>
2022-10-03 10:24:14 +0300

    Релиз pz.7 на старой версии C#

---
+++ pz.7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pz._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            short[] arr = new short[10];
+
+            for (short i = 0; i < arr.GetUpperBound(0); i++)
+                arr[i] = (short)rnd.Next(0, 51);
+
+            // Из-за того, что на компьютере в 407 проблемы с .NET, метод Array.Sort() мне не доступен. Пришлось сортировать так)
+
+            for (short i = 0, tempOne = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (short j = (short)(i + 1); j <= arr.GetUpperBound(0); j++)
+                {
+                    if ((arr[i] % 10) + (arr[i] / 10) > (arr[j] % 10) + (arr[j] / 10))      // Если сумма цифр первого элемента больше суммы цифр следующего
+                    {
+                        tempOne = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tempOne;
+                    }
+                }
+            }
+
+            for (short i = 0, temp = 1; i <= arr.GetUpperBound(0); i++, temp++)
+            {
+                Console.WriteLine($"Элемент массива {temp} = {arr[i]}\tСумма цифр = {(arr[i] % 10) + (arr[i] / 10)}");
+            }
+
+        }
+    }
+}
