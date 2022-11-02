2be7ba7f82fcbdb240363a63f55f8b189055252c
StrvngeMan21 <strangeman21@outlook.com>
2022-11-02 10:01:31 +0500

    pz.11 preAlpha

---
+++ pz.11/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace pz._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "DataStud.txt";
+            StreamReader sr = new StreamReader(path);
+
+            string line;
+            float average = 0.0f;
+            
+            while ((line = sr.ReadLine()) != null)
+            {
+                int temp = 0;
+                for (int iii = 0; iii < line.Length; iii++)
+                {
+                    if (Char.IsNumber(line[iii]))
+                    {
+                        char[] Ctemp = new char[4];
+
+                        for (byte jjj = 0; jjj <= Ctemp.GetUpperBound(0); jjj++)
+                            Ctemp[jjj] = line[iii + jjj];
+
+                        string Stemp = new string(Ctemp);
+                        average += Convert.ToSingle(Stemp);
+
+                        temp++;
+                        iii += 3;
+                    }
+                }
+
+                Console.WriteLine(average/temp);
+                average = 0;
+            }
+            
+
+        }
+    }
+}
