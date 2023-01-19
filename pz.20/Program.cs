8f29b9f3ee8a798205afb7294e13a9e9c9057be9
Пидор Не <strangeman21@outlook.com>
2023-01-19 11:45:12 +0500

    pz 20 адо закончить

---
+++ pz.20/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace pz._20
+{
+    internal class Program
+    {
+        static void stringULTRA(ref string data)
+        {
+            char[] rawData = data.ToCharArray();
+            for (int i = 0; i < rawData.Length; i++)
+            {
+                if (char.IsLetter(rawData[i]))
+                    rawData[i] = char.IsUpper(rawData[i]) ? char.ToLower(rawData[i]) : char.ToUpper(rawData[i]);
+            }
+            data = new string(rawData);
+        }
+
+        static void Main(string[] args)
+        {
+            string data = "";
+            data = Console.ReadLine();
+            stringULTRA(ref data);
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
