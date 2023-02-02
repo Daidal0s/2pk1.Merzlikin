a99bfb1e0f97e52486c7801dd6e68f3dfc7be556
StrvngeMan21 <merzlikin.and@gmail.com>
2023-02-02 13:29:27 +0500

    Добавлен pz.21

---
+++ pz.21/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace pz._21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vec1 = new();
+            Vector vec2 = new(2, 3);
+            Vector vec3 = new(3);
+
+            const float num = 2.5f;
+
+            vec1.PrintInfo();
+            vec1.Multiple(num);
+            Console.WriteLine("Length of the vector = " + vec1.GetLength());
+            vec1.GetMiddle();
+            Console.WriteLine();
+
+            vec2.PrintInfo();
+            vec2.Multiple(num);
+            Console.WriteLine("Length of the vector = " + vec2.GetLength());
+            vec2.GetMiddle();
+            Console.WriteLine();
+
+            vec3.PrintInfo();
+            vec3.Multiple(num);
+            Console.WriteLine("Length of the vector = " + vec3.GetLength());
+            vec3.GetMiddle();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ pz.21/Vector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz._21
+{
+    internal class Vector
+    {
+        private float _x = 0.0f;
+        private float _y = 0.0f;
+
+        public Vector(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Vector(float x)
+        {
+            _x = x;
+        }
+
+        public Vector()
+        {
+
+        }
+
+        public void PrintInfo() => Console.WriteLine($"x = {_x}\ty = {_y} ");
+
+        public void Multiple(float number) => Console.WriteLine($"x * {number} = {_x * number}\ty * {number} = {_y * number} ");
+
+        public void GetMiddle() => Console.WriteLine($"Сoordinates of the middle of the segment: x = {_x / 2}\ty = {_y / 2} ");
+
+        public float GetLength()
+        {
+            return (float)(Math.Sqrt(Math.Pow(_x, 2) + Math.Pow(_y, 2)));
+        }
+    }
+}
