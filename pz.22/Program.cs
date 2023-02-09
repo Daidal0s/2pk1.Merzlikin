ff07ae424b11a5ae12ee6cf54950c30b043f6541
сергей козловцев <blackatom21@mail.ru>
2023-02-09 11:14:27 +0500

    Релиз pz.22

---
+++ pz.22/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace pz._21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const float num = 2.5f;
+
+            Vector vec1 = new();
+            vec1.PrintInfo();
+            vec1.Multiple(num);
+            Console.WriteLine("Length of the vector = " + vec1.GetLength());
+            vec1.GetMiddle();
+            Console.WriteLine();
+
+            Vector vec2 = new(120, 3);
+            vec2.PrintInfo();
+            vec2.Multiple(num);
+            Console.WriteLine("Length of the vector = " + vec2.GetLength());
+            vec2.GetMiddle();
+            Console.WriteLine();
+
+            Vector vec3 = new(12);
+            vec3.PrintInfo();
+            vec3.Multiple(num);
+            Console.WriteLine("Length of the vector = " + vec3.GetLength());
+            vec3.GetMiddle();
+            Console.WriteLine();
+
+            int a = 0;
+        }
+    }
+}
---
+++ pz.22/Vector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz._21
+{
+    internal class Vector
+    {
+        private static int _countOfVec = 0;
+        private static int _countOfZeroVec = 0;
+        private static int _countOfLowLengthVec = 0;
+
+        private float _x = 0.0f;
+        private float _y = 0.0f;
+
+        public Vector(float x, float y)
+        {
+            _x = x;
+            _y = y;
+            this.StaticCount();
+            this.MaxValCheck();
+        }
+
+        public Vector(float x)
+        {
+            _x = x;
+            this.StaticCount();
+            this.MaxValCheck();
+        }
+
+        public Vector()
+        {
+            this.StaticCount();
+            this.MaxValCheck();
+        }
+
+        private void StaticCount()
+        {
+            _countOfVec++;
+            if (this.GetLength() < 10)
+                _countOfLowLengthVec++;
+            if (this.GetLength() == 0.0f)
+                _countOfZeroVec++;
+        }
+        private void MaxValCheck()
+        {
+            _x = _x > 100 ? 100 : _x < -100 ? -100 : _x;
+            _y = _y > 100 ? 100 : _y < -100 ? -100 : _y;
+        }
+
+        public void PrintInfo() => Console.WriteLine($"x = {_x}\ty = {_y}\tCount of vectors = {_countOfVec}\tCount of zero vectors = {_countOfZeroVec}\tCount of with length < 10 = {_countOfLowLengthVec} ");
+
+        public void Multiple(float number) => Console.WriteLine($"x * {number} = {_x * number}\ty * {number} = {_y * number} ");
+
+        public void GetMiddle() => Console.WriteLine($"Сoordinates of the middle of the segment: x = {_x / 2}\ty = {_y / 2} ");
+
+        public float GetLength()
+        {
+            return (float)(Math.Sqrt(Math.Pow(_x, 2) + Math.Pow(_y, 2)));
+        }
+    }    
+}
