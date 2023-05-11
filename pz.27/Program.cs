84c053f7c03643d37853d5dd8b13eb84fbbe4f9e
StrvngeMan21 <strangeman21@outlook.com>
2023-05-11 11:39:48 +0500

    Начало pz.27

---
+++ pz.27/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace pz._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            STUDENT[] STUD1 = new STUDENT[10];
+
+        }
+    }
+}
---
+++ pz.27/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz._27
+{
+    public struct STUDENT
+    {
+        private string NAME = "test";
+        private string GROUP = "test";
+        private byte[] SES = new byte[5];
+
+        private void fillSES()
+        {
+            var rnd = new Random();
+            for (int iii = 0; iii < SES.Length; ++iii)
+            {
+                SES[iii] = Convert.ToByte(rnd.Next(2, 6));
+            }
+        }
+
+        public STUDENT()
+        {
+            fillSES();
+        }
+
+        public STUDENT(string name, string group)
+        {
+            NAME = name;
+            GROUP = group;
+            fillSES();
+        }
+
+        public string Name { get => NAME; set => NAME = Name; }
+        public string Group { get => GROUP; set => GROUP = Group; }
+    }
+}
